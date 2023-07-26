47b50fa336fdf7c2a9057febe7115fbb58a7743e
AleksaLuki <dimirtiy9000@gmail.com>
2023-07-26 16:47:17 +0300

    add verification

--- helpers/index.js
+++ helpers/index.js
@@ -1,9 +1,11 @@
 const HttpError = require("./HttpError");
 const handleMongoosError = require("./handleMongooseError");
 const ctrlWrapper = require('./ctrlWrapper');
+const sendEmail = require('./sendEmail');
 
 module.exports = {
   HttpError,
   handleMongoosError,
   ctrlWrapper,
+  sendEmail,
 };
